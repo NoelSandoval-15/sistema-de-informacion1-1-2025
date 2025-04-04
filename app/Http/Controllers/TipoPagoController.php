8bd40dc5bf16e49b87a330bf06a4068ed6a60b8a
NoelSandoval01 <noelsandoval.002@gmail.com>
2025-04-04 08:29:40 -0400

    first commit

---
+++ app/Http/Controllers/TipoPagoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tipo_pago;
+use Illuminate\Http\Request;
+
+class TipoPagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tipo_pago $tipo_pago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tipo_pago $tipo_pago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tipo_pago $tipo_pago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tipo_pago $tipo_pago)
+    {
+        //
+    }
+}
